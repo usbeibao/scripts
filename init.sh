6a8536e1cb2a06be0728368a22c06179e9b0b9a2
usbeibao <78642967+usbeibao@users.noreply.github.com>
2022-10-18 08:50:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -129,14 +129,14 @@ cloudflare_doh_install() {
 
 worsttrace_install() {
   [[ ! -f /usr/local/bin/worsttrace ]] && wget https://pkg.wtrace.app/linux/worsttrace -O /usr/local/bin/worsttrace
-	[[ ! -f /usr/local/bin/worsttrace ]] && echo -e "${Error} download failed, please check!" && exit 1
-	chmod +x /usr/local/bin/worsttrace
+  [[ ! -f /usr/local/bin/worsttrace ]] && echo -e "${Error} download failed, please check!" && exit 1
+  chmod +x /usr/local/bin/worsttrace
   coloredEcho $GREEN " WorstTrace 安装完成"
 }
 
 besttrace_install() {
-  [[ ! -f /usr/local/bin/besttrace ]] && wget https://github.com/zhucaidan/BestTrace-Linux/raw/master/besttrace -O /usr/local/bin/worsttrace
-	[[ ! -f /usr/local/bin/besttrace ]] && echo -e "${Error} download failed, please check!" && exit 1
+  [[ ! -f /usr/local/bin/besttrace ]] && wget https://github.com/zhucaidan/BestTrace-Linux/raw/master/besttrace -O /usr/local/bin/besttrace
+  [[ ! -f /usr/local/bin/besttrace ]] && echo -e "${Error} download failed, please check!" && exit 1
   chmod +x /usr/local/bin/besttrace
   coloredEcho $GREEN " BestTrace 安装完成"
 }
