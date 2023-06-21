04886c8d89fa0dad95eee881ec38bdb3a62c0e84
usbeibao <78642967+usbeibao@users.noreply.github.com>
2023-06-21 20:52:46 +0800

    Update reality.sh
    set maxTimeDiff to 0

--- reality.sh
+++ reality.sh
@@ -466,7 +466,7 @@ configXray() {
           ],
           "privateKey": "${realityPrivateKey}",
           "publicKey": "${realityPublicKey}",
-          "maxTimeDiff": 70000,
+          "maxTimeDiff": 0,
           "shortIds": [
             ""
           ]
